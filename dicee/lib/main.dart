534f1bca1c50e2feac57708f284da738d7eccf43
AJCordova <archonist252@gmail.com>
2022-04-12 12:29:42 +0800

    Move repeated code into changeDiceFace method.

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -28,6 +28,11 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 5;
   int rightDiceNumber = 1;
 
+  void changeDiceFaces() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -37,7 +42,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
+                  changeDiceFaces();
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -47,7 +52,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
+                  changeDiceFaces();
                 });
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
