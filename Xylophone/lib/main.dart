21a23e8a23b62174a70df798bd1ac35db9c376ae
AJCordova <archonist252@gmail.com>
2022-04-19 19:00:50 +0800

    Update pods with new dependency. Create UI for xylophone controls.

--- Xylophone/lib/main.dart
+++ Xylophone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const Xylophone());
@@ -7,14 +8,87 @@ void main() {
 class Xylophone extends StatelessWidget {
   const Xylophone({Key? key}) : super(key: key);
 
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  child: SizedBox(),
+                  color: Colors.red,
+                  onPressed: () {
+                    playSound(1);
+                  },
+                ),
+              ),
+              Expanded( 
+                child: FlatButton(
+                  child: SizedBox(),
+                  color: Colors.orange,
+                  onPressed: () {
+                    playSound(2);
+                  },
+                ),
+              ),
+              Expanded( 
+                child: FlatButton(
+                  child: SizedBox(),
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playSound(3);
+                  },
+                ),
+              ),
+              Expanded( 
+                child: FlatButton(
+                  child: SizedBox(),
+                  color: Colors.green,
+                  onPressed: () {
+                    playSound(4);
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  child: SizedBox(),
+                  color: Colors.teal,
+                  onPressed: () {
+                    playSound(5);
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  child: SizedBox(),
+                  color: Colors.blue,
+                  onPressed: () {
+                    playSound(6);
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  child: SizedBox(),
+                  color: Colors.purple,
+                  onPressed: () {
+                    playSound(7);
+                  },
+                ),
+              ),
+            ]
+          ),
         ),
-      )
+      ),
     );
   }
 }
